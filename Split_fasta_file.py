2c241f274eefc774d1c7f0bf67f561facf5d9dc2
Manish Yadav <maya620d@tu-dresden.de>
2021-12-10 08:26:08 +0100

    First Commit for Formatting Files

---
+++ Split_fasta_file.py
@@ -0,0 +1,43 @@
+from Bio import SeqIO
+
+
+###The input file should be in Fasta format
+
+input_path = r"C:\Users\maya620d\PycharmProjects\Multiplexing\Ensembles_Data\osativa.fasta"
+
+output_path = r"C:\Users\maya620d\PycharmProjects\Multiplexing\Data\osativa\input_fasta"
+
+def batch_iterator(iterator, batch_size):
+    """Returns lists of length batch_size.
+
+    This can be used on any iterator, for example to batch up
+    SeqRecord objects from Bio.SeqIO.parse(...), or to batch
+    Alignment objects from Bio.AlignIO.parse(...), or simply
+    lines from a file handle.
+
+    This is a generator function, and it returns lists of the
+    entries from the supplied iterator.  Each list will have
+    batch_size entries, although the final list may be shorter.
+    """
+    entry = True  # Make sure we loop once
+    while entry:
+        batch = []
+        while len(batch) < batch_size:
+            try:
+                entry = iterator.__next__()
+            except StopIteration:
+                entry = None
+            if entry is None:
+                # End of file
+                break
+            batch.append(entry)
+        if batch:
+            yield batch
+
+record_iter = SeqIO.parse(input_path, "fasta")
+
+for i, batch in enumerate(batch_iterator(record_iter, 1000)):
+    filename = output_path+"\group_%i.fasta" % (i + 1)
+    with open(filename, "w") as handle:
+        count = SeqIO.write(batch, handle, "fasta")
+    print("Wrote %i records to %s" % (count, filename))
